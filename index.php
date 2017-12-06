3baad7cac7ab2f1f9c4d4992e59b3c8ea6cac427
Li Kwan <likwan.tan00@gmail.com>
2017-12-07 01:59:15 +0800

    I add thise

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title> Lab 4- Question 3 </title>
+
+<link rel="stylesheet" type="text/css" href="style.css"/>
+
+</head>
+
+
+
+<body>
+
+<p class="one">WELCOME TO MMU RESTAURANT PORTAL</p>
+<hr/>
+
+<!-- Home icon by Icons8 -->
+<img class="icon icons8-Home" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAACDUlEQVRoQ+2Y7TVEMRCG360AlaADOtABOqASOkAFdEAHqAQVcN57MsTdm0xmT+YmPzJ/7tndbDLPfOdu4Cv7AB4B8HkK4NPruI3XxkH5ZwBH4Yw3TxhPkNcA8RVA9gAQ5tjDeF4gdwAuABDiJCj+AoAw9wAua8N4gMwh6AUKQ8wNpjZICkIcoMF8Jzyl6qkuMISABlEC0xykFEKDERAx8Pxz0q41PCIQPIQVSXJCcybDjJWNIgWAz3MAq4NcAbgJyrASURGLsLLREJRrALdhD35PWcUjsRK7QAhwbh93kFoQGowrSG2IHMw8Z6oluxdEDoZNVKaDKiDeECmYp5KpubT8noVxnIc9hDnKUp2sayWk+D+O//RKVkpAWO85jvNOsQaEKCwwvMMQJtufNJBWEGaYHEhrCBNMCqQXiGKYJZDeIIpg5iAxBDfQckgrJrV/l06/VQBiRVmV4pcFPYNQt38vMwQkhngHcBhM2atHRMdfGCo6h+A48NE5yEFokjT4BEMQeW1DSkIw/oqnztpJoOwX60UHsONPMARhB2WSC4TpQtMQhEcLzASyJLt4JPXiQGO15GFSrwGyYGarF63rsyHv4ZHSUBkgSxPH8MjIkT8LjBwZOaK15vC7NVSs60cfsV6srBa2rh8eGR5xKg4jtEZojdDSLbDqVbfLi5VuozYrtoylXazaqKmfuqX3Dy0T2rKTah7cAAAAAElFTkSuQmCC" width="50" height="50" "> HOMEPAGE
+
+<!-- Search icon by Icons8 -->
+<a href="searchby.php"><img src="search.png" width="50" height="50"> SEARCH</a>
+
+<!-- Plus icon by Icons8 -->
+<a href="insert.php"><img src="insert.png" width="50" height="50">INSERT</a>
+
+<!-- Delete icon by Icons8 -->
+<a href="remove.php"><img class="icon icons8-Delete" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADQAAAA0CAYAAADFeBvrAAAB6UlEQVRoQ+1a0U3EMAx9twFsABNwbMAKbAATABMgJgAmgA1gBDYAJoARbgPQ07VSMW0Tx3ZBOuer0sUvfu85yan1Cr7jAMA1gBMAfJ4bnwBeANwA4LPLWLmgbEHOANwC2FNibgBcAXhUxo1O9yJEMg/GhM49SHkQYmm9Njgj+dOpY2v5eRC6A3AhsuO+eAbwNuHauitRGXcP4NLitAehD3EAaJKSYvBwOPxrQl8iAZbNlDMyVzrFch0Ok8im4C4LSUiLaY2vVoN3CU+u0n1iqZCWWJYlT0TeYb/GnJpyb7QsHhUzudfmCMlSiEquFXc0950g9N+dkY7+MGXMoSTUugmc4nbPISmc68XX4Ipq/ZpbXQXYkHApRLV+EgJQI0JJdc3viztUWjD69+o/p/3E6ISs+EnIqmB0fDoUrbAVPx2yKhgdnw5FK2zFT4esCkbHp0PRClvx0yGrgtHx6VC0wlb8dMiqYHR8OhStsBU/HbIqGB3v7pDmpWHL3JIgSUgqtA+AbSxLDH6B58fr4Zh9FV3znppNFEcDRH5OP12AFLu6nrpWtX75dwBs1pgcNYQ8Oq283Cx2bNUQYjLSJa8ENThFdwhWS4j2s9SGpadJxjqXZNjZUty7tYT6hFh+bP9aihiJsGOrutvxG6EXuzXKkeXuAAAAAElFTkSuQmCC" width="52" height="52">DELETE RESTAURANT</a>
+
+<!-- Select All icon by Icons8 -->
+<a href="showall.php"><img class="icon icons8-Select-All" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADQAAAA0CAYAAADFeBvrAAABJElEQVRoQ+1a7Q7DIAik7//QW5ZsiZq1wKGUuetvkPug2EYPEXmI7TmGsJJ5L5AlgYkIJCAJNW1X0tlvDo1W296w+6I6YUnoPiNOK9OhgqZ0kPZ3qLoDLny/NqJVcggh64aqFjcGuDC6gt8AyhMaAWokqxEKT7ntCBlbPy0s7FAaUmMhEjIKdVtY2KHVQ0GbsqNyJHSpyIJGo0OtqF41Fhgyd0kSmqvn/NW2dIhf2/Mb5XJFrYvKb6wjOxLyvkPJHaeWC7ecWiE5gISSBXeXCzvE/yGn5tqYVn/wnPXMh8zedT/xXkJdHSS5WsuFCaHKp+QhDqUAQ4vw0BhVbmFeeB9aiA1amoQg2RKT/s+hM3Gh617oNTE0j9fLGvusn0Cpzm7n0BOjcXANPw7DaQAAAABJRU5ErkJggg==" width="52" height="52"> SHOW ALL</a>
+<details>
+<summary>
+<img src="https://maxcdn.icons8.com/Android/PNG/24/Network/wi_fi-24.png" title="Wi-Fi Logo" width="25">
+
+</summary>
+Wifi Password=Wifi@mmu
+</details>
+<hr/>
+
+<center><h1>WELCOME MMU'S ADMIN <img src="banana.gif" width="50" height="50"><h1></center>
+<div><div style="text-align:left;width:350px;"> <h2><a style="text-decoration:none;" href="http://www.zeitverschiebung.net/en/city/1735161"><span style="color:black;">Current local time in</span><br />Kuala Lumpur, Malaysia</a></h2> <iframe src="http://www.zeitverschiebung.net/clock-widget-iframe?language=en&timezone=Asia%2FKuala_Lumpur" width="100%" height="130" frameborder="0" seamless></iframe> <small style="color:black;">&copy; <a href="http://www.zeitverschiebung.net/en/" style="color: gray;">Time Difference</a></small> </div></div>
+
+<p class="align">MMU Restaurant Portal is a sophisticated portal for all the foodies.MMU's was founded in 1989 by a local Toronto based family. Situated on Yonge Street, north of Lawrence Avenue in the heart of a populated upper-middle-class neighbourhood, the concept was simply to create a cozy environment that was well suited to have great conversations, served quality freshly cooked meals and offered great draft beer and spirit selections. After more then two decades of fine-tuning, product development, and loads of hard work, we have what you see today. As of 2015, we have fourteen Gabby's locations and currently serving over 35,000 customers a week.
+
+Today, we still offer a stimulating atmosphere with lots of conversation pieces on the walls and serve excellent quality, fresh cooked meals. We are defined as a main stream casual dining restaurant with an integrated sports bar area that caters to the community and local businesses. We provide friendly, attentive service in a relaxed environment by staff and owners that truly enjoy their job.
+
+Over the years our menu has changed to reflect today's consumer.</p>
+
+
+<audio controls autoplay>
+  
+  <source src="piano.mp3" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio>
+<footer>
+  <p>Coded By: Jeffry Handani 1131121266 , Tan Li Kwan 1122702571 , See Kien Seng 1122702579</p>
+  
+</footer>
+</body>
